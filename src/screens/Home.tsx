55ef36d1c6e07f48a4b0346bd1f96059c4e21c83
DelgadoArlee <delgadoarlee@gmail.com>
2023-09-18 22:13:51 +0800

    Updated home w\ TopAppBar and BottomNavBar component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,23 +1,13 @@
-import * as React from 'react';
-
-import { Appbar } from 'react-native-paper';
+import React from 'react';
+import BottomNavBar from '@/components/BottomNavBar/BottomNavBar';
+import TopAppBar from '@/components/TopAppBar/TopAppBar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Home = () => (
-  <Appbar.Header>
-    <Appbar.BackAction onPress={() => {}} />
-    <Appbar.Content title="Event Hand" />
-    <Appbar.Action icon="calendar" onPress={() => {}} />
-    <Appbar.Action icon="magnify" onPress={() => {}} />
-  </Appbar.Header>
+  <SafeAreaProvider>
+    <TopAppBar />
+    <BottomNavBar />
+  </SafeAreaProvider>
 );
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 
 export default Home;
