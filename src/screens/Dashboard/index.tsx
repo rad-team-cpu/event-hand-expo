92eadd2efa6b2a5d6c2900e1e6ddf6517d9694c4
DelgadoArlee <delgadoarlee@gmail.com>
2023-09-24 17:51:55 +0800

    Temporarily disabled Bottom Navbar

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import BottomNavBar from '@/components/BottomNavBar/BottomNavBar';
+// import BottomNavBar from '@/components/BottomNavBar/BottomNavBar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useTheme } from 'react-native-paper';
 
@@ -7,7 +7,7 @@ const Dashboard = () => {
   const theme = useTheme();
   return (
     <SafeAreaProvider style={{ backgroundColor: theme.colors.background }}>
-      <BottomNavBar />
+      {/* <BottomNavBar /> */}
     </SafeAreaProvider>
   );
 };
