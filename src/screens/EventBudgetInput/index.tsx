d175dcb7a978fef8060d12196abc57a8e922aa72
DelgadoArlee <delgadoarlee@gmail.com>
2023-10-03 17:58:30 +0800

    Added validation to budget Input

--- src/screens/EventBudgetInput/index.tsx
+++ src/screens/EventBudgetInput/index.tsx
@@ -5,18 +5,21 @@ import { Text, TextInput, HelperText, Button } from 'react-native-paper';
 const EventBudgetInput = (props: EventBudgetInputScreenProps) => {
   const { navigation } = props;
   const [error, setError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
   const [input, setInput] = useState<string>('');
 
   const onChange = (text: string) => {
     if (text.match(/^[^\d]+$/g)) {
       setError(true);
+      setErrorMessage('Please Enter a valid number');
+    } else if (parseFloat(text) < 10000) {
+      setError(true);
+      setErrorMessage('Please enter an amount greater than or equal to 10000');
     } else {
       setError(false);
     }
     setInput(text);
   };
-
-  const validInput = !input.match(/^$/) && !error;
 
   return (
     <>
@@ -26,25 +29,26 @@ const EventBudgetInput = (props: EventBudgetInputScreenProps) => {
           justifyContent: 'center',
         }}
       >
-        Please enter your budget
+        Please enter your estimated budget
       </Text>
       <TextInput
         mode="outlined"
-        placeholder="Enter Numeric Values Only"
+        placeholder="max. 1000000"
         placeholderTextColor="#60605e"
+        maxLength={7}
         keyboardType="number-pad"
         onChangeText={onChange}
         error={error}
       />
-      <HelperText type="error" visible={validInput}>
-        Enter a valid number
+      <HelperText type="error" visible={error}>
+        {errorMessage}
       </HelperText>
       <Button
         onPress={() => navigation.navigate('SupplierSelect')}
         mode="outlined"
         buttonColor="#3D50DF"
         textColor="white"
-        disabled={!validInput}
+        disabled={error}
         labelStyle={{ fontWeight: 'bold' }}
         style={{
           position: 'relative',
