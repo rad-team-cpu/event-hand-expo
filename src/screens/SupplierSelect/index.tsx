1b1b27cc22cc1a23a1dc00560ebefdff3f4bf0b9
DelgadoArlee <delgadoarlee@gmail.com>
2023-10-04 09:22:47 +0800

    Added budget sliders on supplier select

--- src/screens/SupplierSelect/index.tsx
+++ src/screens/SupplierSelect/index.tsx
@@ -1,139 +1,229 @@
-import React, { useState } from 'react';
+import React, { useState, Dispatch, SetStateAction } from 'react';
 import { Button, Text, Checkbox } from 'react-native-paper';
 import Slider from '@react-native-community/slider';
 import { SupplierSelectScreenProps } from '@/routes/types';
-import { View } from 'react-native';
+import { GestureResponderEvent, View } from 'react-native';
 
 const SupplierSelect = (props: SupplierSelectScreenProps) => {
-  const [checked, setChecked] = useState(false);
+  const budget = 20000;
+  const [venueChecked, setVenueChecked] = useState<boolean>(false);
+  const [cateringChecked, setCateringChecked] = useState<boolean>(false);
+  const [photographyChecked, setPhotographyChecked] = useState<boolean>(false);
+  const [decorationChecked, setDecorationChecked] = useState<boolean>(false);
+  const [videographyChecked, setVideographyChecked] = useState<boolean>(false);
+  const [venueAmount, setVenueAmount] = useState<number>(budget);
+  const [cateringAmount, setCateringAmount] = useState<number>(budget);
+  const [photographyAmount, setPhotographyAmount] = useState<number>(budget);
+  const [decorationAmount, setDecorationAmount] = useState<number>(budget);
+  const [videographyAmount, setVideographyAmount] = useState<number>(budget);
+  const [supplierChecked, setSupplierChecked] = useState<boolean>(false);
+  const [selectedAll, setSelectedAll] = useState<boolean>(false);
   const { navigation } = props;
+
+  const checkboxStatus = [
+    venueChecked,
+    cateringChecked,
+    photographyChecked,
+    decorationChecked,
+    videographyChecked,
+  ];
+
+  const hasChecked = checkboxStatus.filter((status) => status == false);
+
+  const setAmount =
+    (setAmount: Dispatch<SetStateAction<number>>) => (value: number) =>
+      setAmount(Math.trunc(value));
+
+  const select =
+    (setCheck: Dispatch<SetStateAction<boolean>>) =>
+    (status: boolean) =>
+    (event: GestureResponderEvent) =>
+      setCheck(!status);
+
+  const selectAll = (event: GestureResponderEvent) => {
+    if (selectedAll == false) {
+      setVenueChecked(true);
+      setCateringChecked(true);
+      setPhotographyChecked(true);
+      setDecorationChecked(true);
+      setVideographyChecked(true);
+      setSelectedAll(true);
+    } else {
+      setVenueChecked(false);
+      setCateringChecked(false);
+      setPhotographyChecked(false);
+      setDecorationChecked(false);
+      setVideographyChecked(false);
+      setSelectedAll(false);
+    }
+  };
+
+  const is = (checked: boolean) => (checked ? 'checked' : 'unchecked');
+
+  const disableButton =
+    !venueChecked &&
+    !cateringChecked &&
+    !photographyChecked &&
+    !decorationChecked;
+
   return (
     <>
       <Text
         variant="headlineLarge"
         style={{ textAlign: 'center', justifyContent: 'space-evenly' }}
       >
-        5000
+        {budget}
       </Text>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingHorizontal: 5,
-        }}
-      >
-        <Text variant="titleLarge" style={{ margin: 5 }}>
-          Venue
-        </Text>
-        <Checkbox
-          status={checked ? 'checked' : 'unchecked'}
-          onPress={() => {
-            setChecked(!checked);
+      <View>
+        <View
+          style={{
+            flexDirection: 'row',
+
+            paddingHorizontal: 5,
           }}
-        />
-        <Slider
-          style={{ width: 200, height: 40 }}
-          minimumValue={0}
-          maximumValue={1}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-        />
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingHorizontal: 5,
-        }}
-      >
-        <Text variant="titleLarge" style={{ margin: 5 }}>
-          Catering
-        </Text>
-        <Checkbox
-          status={checked ? 'checked' : 'unchecked'}
-          onPress={() => {
-            setChecked(!checked);
+        >
+          <Text variant="titleLarge" style={{ margin: 5 }}>
+            Select All
+          </Text>
+          <Checkbox status={is(selectedAll)} onPress={selectAll} />
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingHorizontal: 5,
           }}
-        />
-        <Slider
-          style={{ width: 200, height: 40 }}
-          minimumValue={0}
-          maximumValue={1}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-        />
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingHorizontal: 5,
-        }}
-      >
-        <Text variant="titleLarge" style={{ margin: 5 }}>
-          Decorations
-        </Text>
-        <Checkbox
-          status={checked ? 'checked' : 'unchecked'}
-          onPress={() => {
-            setChecked(!checked);
+        >
+          <Text variant="titleLarge" style={{ margin: 5 }}>
+            Venue
+          </Text>
+          <Checkbox
+            status={is(venueChecked)}
+            onPress={select(setVenueChecked)(venueChecked)}
+          />
+          <Slider
+            style={{ width: 200, height: 40 }}
+            minimumValue={1}
+            maximumValue={budget}
+            value={venueAmount}
+            disabled={!venueChecked}
+            minimumTrackTintColor="#3D50DF"
+            maximumTrackTintColor="#000000"
+            thumbTintColor="#FFFFFF"
+            onValueChange={setAmount(setVenueAmount)}
+          />
+          <Text style={{ margin: 5 }}>P{venueAmount}</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingHorizontal: 5,
           }}
-        />
-        <Slider
-          style={{ width: 200, height: 40 }}
-          minimumValue={0}
-          maximumValue={1}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-        />
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingHorizontal: 5,
-        }}
-      >
-        <Text variant="titleLarge" style={{ margin: 5 }}>
-          Photography
-        </Text>
-        <Checkbox
-          status={checked ? 'checked' : 'unchecked'}
-          onPress={() => {
-            setChecked(!checked);
+        >
+          <Text variant="titleLarge" style={{ margin: 5 }}>
+            Catering
+          </Text>
+          <Checkbox
+            status={is(cateringChecked)}
+            onPress={select(setCateringChecked)(cateringChecked)}
+          />
+          <Slider
+            style={{ width: 200, height: 40 }}
+            minimumValue={1}
+            maximumValue={budget}
+            value={cateringAmount}
+            disabled={!cateringChecked}
+            minimumTrackTintColor="#3D50DF"
+            maximumTrackTintColor="#000000"
+            thumbTintColor="#FFFFFF"
+            onValueChange={setAmount(setCateringAmount)}
+          />
+          <Text style={{ margin: 5 }}>P{cateringAmount}</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingHorizontal: 5,
           }}
-        />
-        <Slider
-          style={{ width: 200, height: 40 }}
-          minimumValue={0}
-          maximumValue={1}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-        />
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingHorizontal: 5,
-        }}
-      >
-        <Text variant="titleLarge" style={{ margin: 5 }}>
-          Videography
-        </Text>
-        <Checkbox
-          status={checked ? 'checked' : 'unchecked'}
-          onPress={() => {
-            setChecked(!checked);
+        >
+          <Text variant="titleLarge" style={{ margin: 5 }}>
+            Photography
+          </Text>
+          <Checkbox
+            status={is(photographyChecked)}
+            onPress={select(setPhotographyChecked)(photographyChecked)}
+          />
+          <Slider
+            style={{ width: 200, height: 40 }}
+            minimumValue={1}
+            maximumValue={budget}
+            value={photographyAmount}
+            disabled={!photographyChecked}
+            minimumTrackTintColor="#3D50DF"
+            maximumTrackTintColor="#000000"
+            thumbTintColor="#FFFFFF"
+            onValueChange={(value: number) => {
+              setPhotographyAmount(Math.trunc(value));
+            }}
+          />
+          <Text style={{ margin: 5 }}>P{photographyAmount}</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingHorizontal: 5,
           }}
-        />
-        <Slider
-          style={{ width: 200, height: 40 }}
-          minimumValue={0}
-          maximumValue={1}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-        />
+        >
+          <Text variant="titleLarge" style={{ margin: 5 }}>
+            Decoration
+          </Text>
+          <Checkbox
+            status={is(decorationChecked)}
+            onPress={select(setDecorationChecked)(decorationChecked)}
+          />
+          <Slider
+            style={{ width: 200, height: 40 }}
+            minimumValue={1}
+            maximumValue={budget}
+            value={decorationAmount}
+            disabled={!decorationChecked}
+            minimumTrackTintColor="#3D50DF"
+            maximumTrackTintColor="#000000"
+            thumbTintColor="#FFFFFF"
+            onValueChange={setAmount(setDecorationAmount)}
+          />
+          <Text style={{ margin: 5 }}>P{decorationAmount}</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingHorizontal: 5,
+          }}
+        >
+          <Text variant="titleLarge" style={{ margin: 5 }}>
+            Videography
+          </Text>
+          <Checkbox
+            status={is(videographyChecked)}
+            onPress={select(setVideographyChecked)(videographyChecked)}
+          />
+          <Slider
+            style={{ width: 200, height: 40 }}
+            minimumValue={1}
+            maximumValue={budget}
+            value={videographyAmount}
+            disabled={!videographyChecked}
+            minimumTrackTintColor="#3D50DF"
+            maximumTrackTintColor="#000000"
+            thumbTintColor="#FFFFFF"
+            onValueChange={setAmount(setVideographyAmount)}
+          />
+          <Text style={{ margin: 5 }}>P{videographyAmount}</Text>
+        </View>
       </View>
       <Button
         onPress={() => navigation.navigate('Checklist')}
@@ -141,6 +231,7 @@ const SupplierSelect = (props: SupplierSelectScreenProps) => {
         mode="outlined"
         buttonColor="#3D50DF"
         textColor="white"
+        disabled={disableButton}
         labelStyle={{ fontWeight: 'bold' }}
         style={{
           position: 'absolute',
